294d8a5a0c3281f0e5be0e471c9cce52211c7f40
Iamneo <techadmin@iamneo.ai>
2023-08-07 13:46:41 +0000

    Backup Commit

--- reactapp2/src/App.js
+++ reactapp2/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+
 import "./App.css";
 import Banner from "./components/UI/Banner/Banner";
+
 import Button from "./components/UI/Button/Button";
 import Card from "./components/UI/Card/Card";
 import questions from "./Data/data";
@@ -17,7 +19,8 @@ function App() {
 
   function func(correct) {
     if (correct === true) {
-      setQuestionsCorrect((prevCorrect) => prevCorrect + 1);
+      setQuestionsCorrect(questionsCorrect + 1);
+      console.log("questionsCorrect: " + questionsCorrect);
     }
   }
 
@@ -26,51 +29,40 @@ function App() {
     setEnd(true);
   }
 
-  // Helper function to split questions into groups of 3, 4, or 5
-  function splitQuestionsIntoRows(questions, rowSize) {
-    const rows = [];
-    for (let i = 0; i < questions.length; i += rowSize) {
-      const row = questions.slice(i, i + rowSize);
-      rows.push(row);
-    }
-    return rows;
-  }
-
   return (
     <div className="app">
       <h1>Quizz App</h1>
+
       {start ? (
         <div className="container">
           <div className="card-container">
-            {splitQuestionsIntoRows(questions, 3).map((row, rowIndex) => (
-              <div key={rowIndex} className="question-row">
-                {row.map(
-                  ({
-                    questionId,
-                    question,
-                    option1,
-                    option2,
-                    option3,
-                    option4,
-                    answer,
-                  }) => (
-                    <Card
-                      key={questionId}
-                      question={question}
-                      correctAnswerMarkUpdate={func}
-                      attempt={func}
-                      options={{
-                        option1: option1,
-                        option2: option2,
-                        option3: option3,
-                        option4: option4,
-                      }}
-                      answer={answer}
-                    />
-                  )
-                )}
-              </div>
-            ))}
+            {questions.map(
+              ({
+                questionId,
+                question,
+                option1,
+                option2,
+                option3,
+                option4,
+                answer,
+              }) => {
+                return (
+                  <Card
+                    key={questionId}
+                    question={question}
+                    correctAnswerMarkUpdate={func}
+                    attempt={func}
+                    options={{
+                      option1: option1,
+                      option2: option2,
+                      option3: option3,
+                      option4: option4,
+                    }}
+                    answer={answer}
+                  />
+                );
+              }
+            )}
           </div>
           <Button onClick={showResults}>{"Show results"}</Button>
         </div>
